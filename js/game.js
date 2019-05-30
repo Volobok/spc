f4d6c06c0ca1444327b7db78c9994f6d88e6362c
Volobok <50802527+Volobok@users.noreply.github.com>
2019-05-30 12:38:27 +0300

    Add files via upload

--- js/game.js
+++ js/game.js
@@ -20,15 +20,60 @@ var score_audio = new Audio();
 fly.src = "audio/fly.mp3";
 score_audio.src = "audio/score.mp3";
 
-var gap = 100;
+var gap = 175;
 
 // Кнопка фор птичка
-document.addEventListener("keydown", moveUp)
+document.addEventListener("keydown", moveRect);
+
 
 function moveUp() {
   yPos -= 25;
   fly.play();
 }
+
+
+function moveRect(e){
+
+    switch(e.keyCode){
+
+        case 37:  // если нажата клавиша влево
+            if (xPos < 25) {
+              xPos = 0;
+            } else {
+              xPos -= 25;
+            }
+
+            fly.play();
+            break;
+        case 38:   // если нажата клавиша вверх
+            if (yPos < 25) {
+              yPos = 0;
+            } else {
+              yPos -= 25;
+            }
+
+            fly.play();
+            break;
+        case 39:   // если нажата клавиша вправо
+            if (xPos > 450) {
+              xPos = 480;
+            } else {
+              xPos += 25;
+            }
+           fly.play();
+            break;
+        case 40:   // если нажата клавиша вниз
+            if (yPos > 900) {
+              yPos = 910;
+            } else {
+              yPos +=25
+              }
+
+            fly.play();
+            break;
+    }
+}
+
 
 //BLOKU
 var pipe = [];
@@ -49,31 +94,42 @@ var yPos = 850;
 function draw() {
   ctx.drawImage(bg, 0, 0);
 
-  for(var i = 0; i < pipe.length; i++) {
+  //for(var i = 0; i < pipe.length; i++)
+
+  var i = 0;
+  while (i < pipe.length)
+  {
     ctx.drawImage(pipeLeft, pipe[i].x, pipe[i].y);
-    //ctx.drawImage(pipeRight, pipe[i].x, pipe[i].y + pipeUp.height + gap);
+    ctx.drawImage(pipeRight, pipe[i].x + pipeLeft.width + gap, pipe[i].y);
 
     pipe[i].y++;
 
     if(pipe[i].y == 300) {
       pipe.push({
-        x : 0,
-        y : 0
-      //  y : Math.floor(Math.random() * pipeUp.height) - pipeUp.height
+        x : Math.floor(Math.random() * pipeLeft.width) - pipeLeft.width,
+        y : 0,
       });
     }
 
-  //  if(xPos + bird.width >= pipe[i].x
-  //    && xPos <= pipe[i].x + pipeUp.width
-  //    && (yPos <= pipe[i].y + pipeUp.height
-  //      || yPos + bird.height >= pipe[i].y + pipeUp.height + gap) || yPos + bird.height >= cvs.height - fg.height  ) {
-  //        location.reload(); // Перезагрузка страницы
-  //      }
+   if(yPos <= pipe[i].y + pipeLeft.height
+      && yPos + bird.height >= pipe[i].y
+      && !(xPos > pipe[i].x + pipeLeft.width
+      && xPos + bird.width < pipe[i].x + pipeLeft.width + gap)) {
+          location.reload(); // Пере
+    }
 
-    if(pipe[i].x == 5) {
+  //  if(xPos + bird.width >= pipe[i].x
+    //  && xPos <= pipe[i].x  + pipeUp.width
+      //&& (yPos <= pipe[i].y + pipeUp.height
+        //|| yPos + bird.height >= pipe[i].y + pipeUp.height + gap) || yPos + bird.height >= cvs.height - fg.height  ) {
+          //location.reload(); // Перезагрузка страницы
+      //}
+
+   if(pipe[i].x == 5) {
       score++;
       score_audio.play();
     }
+    i += 1;
   }
 
   //ctx.drawImage(fg, 0, cvs.height - fg.height);
